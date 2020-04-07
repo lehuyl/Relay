951dc5eeee38aa859a2883b6b40635b77cb35fc3
foamy <martin.red.202@gmail.com>
2020-04-06 21:42:37 -0700

    update main page

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-const { override, fixBabelImports, addLessLoader} = require('customize-cra');
-
-
-module.exports = override(
- fixBabelImports('import', {
-=======
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
  module.exports = override(
    fixBabelImports('import', {
->>>>>>> martin
      libraryName: 'antd',
      libraryDirectory: 'es',
      style: true,
    }),
    addLessLoader({
-<<<<<<< HEAD
-       javascriptEnabled: true,
-       modifyVars: { 
-           '@primary-color': '#1DA57A' },
-     }),
-=======
        javascriptEnabled: true, 
        modifyVars:{
         '@primary-color': '#1DA57A' 
        }
    }),
->>>>>>> martin
  );
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
+import {Layout, Avatar, Menu, Breadcrumb} from 'antd';
 import { Button } from 'antd';
+import Title from "antd/lib/typography/Title";
+import '../src/App.scss'
+
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+
+
+
 
 const App = () => {
   return (
-    <main
-      style={{
-        height: '500px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Button type="primary">Hello, Ant Design!</Button>
-      <a href="foo.bar">I'm a link. Click me, please!</a>
-    </main>
+  <div>
+    <Layout>
+    <Header style={{padding:10}}>
+      <Avatar style={{float: 'right'}} icon="user"/>
+      <Title style={{color: 'white'}} level={3}> Relay </Title>
+    </Header>
+    <Layout>
+      <Sider>
+      <Menu>
+          <Menu.Item>
+            Group 1
+         </Menu.Item>
+        </Menu>
+        
+      </Sider>
+    <Layout>
+      <Content>
+        <div className="site-layout-content"> Content </div>
+        </Content>
+      <Footer style={{textAlign: 'center'}}> 
+      xd lmao ©2020 Created by Steven and Martin
+      </Footer>
+  </Layout>
+  </Layout>
+  </Layout>
+</div>
   );
 };
 
